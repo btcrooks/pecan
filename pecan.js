41a04e7469959be69e6218cb4628787b843a8417
btcrooks <brandoncrooks@live.com>
2015-09-28 06:12:24 -0400

    Remove ghDownload

--- pecan.js
+++ pecan.js
@@ -3,9 +3,6 @@
 var fs = require('fs'),
     url = require('url'),
     http = require('http'),
-    exec = require('child_process').exec,
-    spawn = require('child_process').spawn,
-    ghDownload = require('github-download'),
     pecanApp = require('commander'),
     pecanInfo = require('./package.json');
 
@@ -18,24 +15,6 @@ pecanApp
   .action(function(){
     console.log('Creating new project');
     // PyProcessing Repo
-    ghDownload({user: 'jdf', repo: 'processing.py', ref: 'master'}, process.cwd())
-    .on('dir', function(dir) {
-      console.log(dir)
-    })
-    .on('file', function(file) {
-      console.log(file)
-    })
-    .on('zip', function(zipUrl) { //only emitted if Github API limit is reached and the zip file is downloaded
-      console.log(zipUrl)
-    })
-    .on('error', function(err) {
-      console.error(err)
-    })
-    .on('end', function() {
-      exec('tree', function(err, stdout, sderr) {
-        console.log(stdout)
-      })
-    })
   }).on('--help', function() {
     console.log(' Run to create new projects... its simple.');
   });
