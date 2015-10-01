ab12032fc5b0fd6959deb014583ce13988045fa1
btcrooks <brandoncrooks@live.com>
2015-09-30 23:31:56 -0400

    Remove writeStream

--- pecan-init.js
+++ pecan-init.js
@@ -7,6 +7,7 @@ var fs        = require('fs'),
     download  = require('download'),
     pecanApp  = require('commander'),
     pecanInfo = require('./package.json');
+
 var pypRepo = 'https://github.com/jdf/processing.py/archive/master.zip',
     dir = './processing.py-master';
 
@@ -38,26 +39,8 @@ var setupPecanProject = function () {
 
   prompt.get(appInfo, function (err, result){
     // Creat Pecan run file
-    var wstream = fs.createWriteStream('./run.pecan');
-    // File Header
-    wstream.write('#/usr/bin/env bash \n');
-    wstream.write('#\n');
-    wstream.write('# Pecan run file\n');
-    wstream.write('# Contributors: ' + result.contributors + '\n');
-    wstream.write('# Project Entrypoint: ' + result.projectEntrypoint + '\n');
-    // Pretty console stuff
-    wstream.write('echo "Openning Console..."\n');
-    wstream.write('echo "***Console***"\n');
-    wstream.write(
-      './PyProcessing/processing-py.sh' + result.projectEntrypoint + '&& echo "************\nConsole Closed"\n');
-    wstream.write('exit 0 \n');
-    wstream.on('error', function (err) {
-      console.log(err);
-    });
-    wstream.end( function () {
-      console.log('Created pecan run file!');
-      console.log('run `$ pecan run` to run your PyProcessing project.');
-    });
+    // result.projectEntrypoint
+    // result.contributors
   });
 };
 
