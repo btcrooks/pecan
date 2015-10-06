ce046f72b74202814fd1b41c0760b37f3876495f
btcrooks <brandoncrooks@live.com>
2015-10-06 15:15:12 -0400

    Minor updates

--- pecan-run.js
+++ pecan-run.js
@@ -6,17 +6,24 @@ var fs        = require('fs'),
     pecanApp  = require('commander'),
     pecanInfo = require('./package.json');
 
+var pecanLib    = process.env.HOME + '/.pecan',
+    pypFolder   = '/processing.py-master',
+    pecanConfig = './config.pecan';
+
 pecanApp
   .parse(process.argv);
 
 // Check it project already exists
-if (!fs.existsSync('./run.pecan')) {
+if (!fs.existsSync(pecanConfig)) {
   console.log('Pecan project doesn\'t exist!');
   // TODO: Prompt user to create a new project
   console.log('Run `$ pecan init` to get started');
   process.exit(1);
 } else {
   console.log('Running PyProcessing app.');
-  var child = shjs.exec('bash ./processing.py-master/processing-py.sh main.py');
+  //
+  // Hack... need to change this.
+  //-----------------------------
+  var child = shjs.exec('bash ' + pecanLib + pypFolder + '/processing-py.sh main.py');
   shjs.exit(0);
 }
